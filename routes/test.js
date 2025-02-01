e336fab0293c2edcba9980bc4feb3969ba776a5f
Michael McMullen <mbmcmullen27@gmail.com>
2025-02-01 20:34:44 +0100

    initial commit

---
+++ routes/test.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var handlebars = require('hbs').handlebars;
+var fs = require('fs');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    console.log(process.cwd())
+    var template = handlebars.compile(fs.readFileSync('views/test.hbs','utf8'))
+    console.log(template({title: "stinky", data: "farts"}))
+});
+
+module.exports = router;
