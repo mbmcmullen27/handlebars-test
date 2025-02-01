67dbc9390c30c806a38bf381cd940233215bb067
Michael McMullen <mbmcmullen27@gmail.com>
2025-02-01 22:22:36 +0100

    testing octokit

--- routes/publish.js
+++ routes/publish.js
@@ -2,14 +2,15 @@ var express = require('express');
 var handlebars = require('hbs').handlebars;
 var fs = require('fs');
 var router = express.Router();
-var App = require('@octokit').App
+var App = require('octokit').App
 
 
 // login (how do I persist a user sesion?)
-const app = new App({
-  appId: 1130592,
-  privateKey: PRIVATE_KEY,
-});
+// const app = new App({
+//   appId: 1130592,
+//   privateKey: fs.readFileSync('fishbars-handle-app.2025-02-01.private-key.pem', 'utf-8'),
+// });
+
 
 // fetch template from repo
 
@@ -18,11 +19,23 @@ const app = new App({
 // attempt to create the repo with octokit
 
 // send errors
-
-router.get('/', function(req, res, next) {
-    console.log(process.cwd())
-    var template = handlebars.compile(fs.readFileSync('views/test.hbs','utf8'))
-    console.log(template({title: "stinky", data: "farts"}))
+router.get('/', async function(req, res, next) {
+    const app = new App({ appId:"1130592", privateKey: fs.readFileSync('fishbars-handle-app.2025-02-01.private-key.pem', 'utf-8')});
+    const { data: slug } = await app.octokit.rest.apps.getAuthenticated();
+    const octokit = await app.getInstallationOctokit(12360391574);
+    // const octokit = await app.getInstallationOctokit(123);
+    await octokit.rest.issues.create({
+        owner: "mbmcmullen27",
+        repo: "handlebars-test",
+        title: "Hello world from " + slug,
+    });
+    // console.log(
+    //     octokit.rest.repos.get({
+    //         owner: 'mbmcmullen27',
+    //         repo: 'handlebars-test',
+    //     })
+    // )
+    res.send("WHATEVER!")
 });
 
 module.exports = router;
